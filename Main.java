6d7cad8b0417a0946ebef257907b6aa4f0ca94a7
SimpleThingsEveryday <nikitabagrov59@gmail.com>
2017-01-20 14:02:29 +0500

    InitialCommitInvertArr

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(int[] data)  {
+            for (int k = 0; k < data.length /2 ; k++)
+            {
+                int tmp = data[k]; // tmp = data[0]=  1
+                data[k] = data[data.length - k -1]; // data[0] = data[10 - 0 - 1] = data[9] = 0
+                data[data.length-1-k] = tmp; //data[9] = 1;
+            }
+
+
+        }
+    }
+
+// data = {1,2,3,4,5,6,7,8,9,0}
+
+
+
